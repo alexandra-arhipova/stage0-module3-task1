770a7d5c69da4e9ef3bdbf2665d6bdb161711a32
alexandra-arhipova <alexandra9.arhipova@gmail.com>
2023-10-23 19:47:24 +0300

    module 3 task 1 comleted.

--- src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
@@ -2,6 +2,9 @@ package school.mjc.stage0.module3.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("im update, can be compiled and can write to console.");
+        String i = "im update, ";
+        String c = "can be compiled ";
+        String a = "and can write to console.";
+        System.out.println(i + c + a);
     }
 }
