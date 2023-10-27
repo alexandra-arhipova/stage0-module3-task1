268c5ce4b063dc872946ae24e7275909355b9e5c
alexandra-arhipova <alexandra9.arhipova@gmail.com>
2023-10-27 15:47:24 +0300

    module3 task1 fixed issues

--- src/main/java/school/mjc/stage0/module3/task1/FillGapsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/FillGapsApp.java
@@ -2,7 +2,6 @@ package school.mjc.stage0.module3.task1;
 
 public class FillGapsApp {
     public static void main(String[] args) {
-            String fixed = "Fixed";
-        System.out.println(fixed);
+        System.out.println("Fixed");
      }
 }
