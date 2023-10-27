6b30ea938aa837391c3f6b437ecfdc5b444649d1
alexandra-arhipova <alexandra9.arhipova@gmail.com>
2023-10-27 15:26:37 +0300

    module3 task1 fixed issues

--- src/main/java/school/mjc/stage0/module3/task1/FillGapsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/FillGapsApp.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.module3.task1;
 
 public class FillGapsApp {
     public static void main(String[] args) {
-            String fixed = ("fixed");
+            String fixed = "fixed";
         System.out.println(fixed);
      }
 }
--- src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
@@ -2,9 +2,10 @@ package school.mjc.stage0.module3.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        String i = "im update, ";
-        String c = "can be compiled ";
-        String a = "and can write to console.";
-        System.out.println(i + c + a);
+        String i = "I'm updated";
+        String c = "can be compiled. ";
+        String a = "And can write to console";
+        System.out.println(i);
+        System.out.println(c + a);
     }
 }
