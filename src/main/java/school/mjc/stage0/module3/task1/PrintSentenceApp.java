0f12953bedf20b90e868f221b165fdca41a37783
alexandra-arhipova <alexandra9.arhipova@gmail.com>
2023-10-27 15:31:25 +0300

    module3 task1 fixed issues

--- src/main/java/school/mjc/stage0/module3/task1/PrintSentenceApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintSentenceApp.java
@@ -2,7 +2,6 @@ package school.mjc.stage0.module3.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
-        String s = "This is my first Java program.";
-        System.out.println(s);
+        System.out.println("This is my first Java program");
     }
 }
